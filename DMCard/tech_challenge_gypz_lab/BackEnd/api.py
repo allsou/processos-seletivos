56c8ac5a72dd5dfc6afedc0f276331f208330835
V3nd3d0r <allan.santos22@fatec.sp.gov.br>
2019-08-31 18:26:30 -0300

    CORS Policy

--- DMCard/tech_challenge_gypz_lab/BackEnd/api.py
+++ DMCard/tech_challenge_gypz_lab/BackEnd/api.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
+from flask_cors import CORS
 from resources.cardRequestResources import CardRequestMaintenceResource, CardRequestResource
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
-@app.route('/')
-def inddex():
-    return render_template('index.html')
-
-api.add_resource(CardRequestResource, '/cardrequest')
+api.add_resource(CardRequestResource, '/cardrequest/')
 api.add_resource(CardRequestMaintenceResource, '/cardrequest/<int:req_id>')
 if __name__ == '__main__':
     app.run(debug=True)
